51d4aaa6d73b24743ce7dc66e35bf3d7d2acca2b
Christian <brewer.christiang@gmail.com>
2023-12-21 11:15:55 -0500

    creating navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image'
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
+      <>
+      <Navbar />
     <p>streamly</p>
+      </>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,29 @@
 "use client";
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuList,
+    NavigationMenuTrigger
+} from "@/components/ui/navigation-menu";
+
 const Navbar = () => {
     return (
-        <div className="flex">
-
+        <div className="sticky z-50 top-0 inset-x-0 h-16 ">
+        <header className="relative">
+            <div className="">
+                { /* Logo */ }
+            </div>
+            <div>
+                { /* Nav menu */ }
+                <NavigationMenu>
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger>Platforms</NavigationMenuTrigger>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </div>
+        </header>
         </div>
     )
 };
