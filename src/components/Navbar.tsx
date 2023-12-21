fc8b09d0c6269d4d3815b50c7eea7b2850c8753e
Christian <brewer.christiang@gmail.com>
2023-12-21 15:42:12 -0500

    adds platform selector to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,50 @@
 "use client";
 import {
-    NavigationMenu,
-    NavigationMenuItem,
+    NavigationMenu, NavigationMenuContent,
+    NavigationMenuItem, NavigationMenuLink,
     NavigationMenuList,
     NavigationMenuTrigger
 } from "@/components/ui/navigation-menu";
+import { platforms } from "@/lib/platforms";
+import * as React from "react";
+import Link from "next/link"
+import ListItem from "@/components/ListItem";
+
+
 
 const Navbar = () => {
     return (
         <div className="sticky z-50 top-0 inset-x-0 h-16 ">
-        <header className="relative">
-            <div className="">
-                { /* Logo */ }
-            </div>
-            <div>
-                { /* Nav menu */ }
-                <NavigationMenu>
-                    <NavigationMenuList>
-                        <NavigationMenuItem>
-                            <NavigationMenuTrigger>Platforms</NavigationMenuTrigger>
-                        </NavigationMenuItem>
-                    </NavigationMenuList>
-                </NavigationMenu>
-            </div>
-        </header>
+            <header className="relative">
+                <div className="">
+                    { /* Logo */}
+                </div>
+                <div>
+                    { /* Nav menu */}
+                    <NavigationMenu>
+                        <NavigationMenuList>
+                            <NavigationMenuItem>
+                                <NavigationMenuTrigger>Platforms</NavigationMenuTrigger>
+                                <NavigationMenuContent>
+                                    <ul className="grid w-[400] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                                        {platforms.map((platform) => (
+                                            <ListItem
+                                            key={platform.name}
+                                            title={platform.name}
+                                            href={platform.href}
+                                            >
+                                                {platform.tagLine}
+                                            </ListItem>
+                                        ))}
+                                    </ul>
+                                </NavigationMenuContent>
+                            </NavigationMenuItem>
+                        </NavigationMenuList>
+                    </NavigationMenu>
+                </div>
+            </header>
         </div>
-    )
+    );
 };
 
 export default Navbar;
---
+++ src/lib/platforms.ts
@@ -0,0 +1,36 @@
+export const platforms: {
+    name: string;
+    href: string;
+    tagLine: string;
+}[] = [
+    {
+        name: "Netflix",
+        href: "/netflix",
+        tagLine: "See what's next."
+    },
+    {
+        name: "Hulu",
+        href: "/hulu",
+        tagLine: "Come TV with us."
+    },
+    {
+        name: "Max",
+        href: "/max",
+        tagLine: "The one to watch"
+    },
+    {
+        name: "Prime Video",
+        href: "/prime",
+        tagLine: "See Where It Takes You."
+    },
+    {
+        name: "Disney+",
+        href: "/disney",
+        tagLine: "Where timeless characters meet the heroes of today."
+    },
+    {
+        name: "Peacock",
+        href: "/peacock",
+        tagLine: "Can't not watch."
+    },
+]
